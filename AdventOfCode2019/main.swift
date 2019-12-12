2b8f1647ffb20c80472361927bbc283c44359222
STH <sth@adorsys.de>
2019-12-12 11:21:48 +0100

    Initial Commit

---
+++ AdventOfCode2019/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode2019
+//
+//  Created by Stefan Haßferter on 12.12.19.
+//  Copyright © 2019 Stefan Haßferter. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
